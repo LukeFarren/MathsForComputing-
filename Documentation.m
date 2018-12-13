e8f90aee0ac122946acd71e8d397632cba76dfeb
Luke Farren <lukefarren@btinternet.com>
2018-12-13 23:06:44 +0000

    Update and rename README.md to Documentation.m

---
+++ Documentation.m
@@ -0,0 +1,128 @@
+## Unit 11: Maths for Computing. 
+Luke Farren
+
+
+## **Part 1: A**
+
+For part A both the Lowest Common Multiple and the Highest Common Factor has to be found for both eight and twelve. 
+
+To find the Lowest Common Multiple we have to know the sequence of multiplication for both eight and twelve.
+
+ **Multiples of Eight:**	
+1 x 8 = 8      
+2 x 8 = 16     
+3 x 8 = 24     
+4 x 8 = 32
+
+**Multiples of Twelve:**
+1 x 12 = 12
+2 x 12 = 24
+3 x 12 = 36
+4 x 12 = 48
+
+Therefore, the Lowest Common Multiple of both eight and twelve equals 24. 
+
+To find the Highest Common Factor we have to factor both eight and twelve.  
+
+**Factors of Eight:**
+
+1 x 8 = 8
+2 x 4 = 8
+
+1, 2, 4, 8
+
+**Factors of Twelve:**
+1 × 12 = 12
+2 × 6 = 12
+3 × 4 = 12
+
+1, 2, 3, 4, 6, 12
+
+Therefore, the Highest Common Factor of both eight and twelve equals 4.
+
+Here is the application that was created using Euclid's algorithm calculating both the Lowest Common Multiple and the Highest Common Factor.
+
+[HotDogApplication](https://github.com/LukeFarren/MathsForComputing-/blob/master/HotDogApplication)
+
+## **Part 1: B**
+
+Using Euclid's Algorithm to calculate the Highest Common Factor of both 660 an 288.  
+
+660 = (288 x 2) + 84
+288 = (84 x 3) + 36
+84 = (36 x 2) + 12
+36 = (12 x 3) + 0
+
+There for the Highest Common Factor for both 660 and 288 equals twelve. 
+
+## **Part 2: A**
+
+**Arithmetic Sequence.**
+![Formulae](https://github.com/LukeFarren/MathsForComputing-/blob/master/Formulae.png)
+
+a1=1  ,  d=1  ,  n=42
+
+![Workings](https://github.com/LukeFarren/MathsForComputing-/blob/master/Workings.png)
+
+S42 = 903
+
+42 Days = £9.03
+
+## **Part 2: B**
+
+**Geometric Sequence.** 
+
+![Formulae2](https://github.com/LukeFarren/MathsForComputing-/blob/master/Formulae2.png)
+
+![Workings2](https://github.com/LukeFarren/MathsForComputing-/blob/master/Workings2.png)
+
+26th term = 306.68
+
+306.68  minutes = 18401 seconds. 
+
+## Part 3: A
+
+**Multiplicative Inverse Sequences.** 
+
+A) 1 mod 9
+1 x 0 mod 9 = 0 
+1 x 1 mod 9 = 1
+
+B) 5 mod 9 
+5 x 0 mod 9 = 0
+5 x 1 mod 9 = 5
+5 x 2 mod 9 = 1
+
+C) 7 mod 9
+7 x 0 mod 9 = 0
+7 x 1 mod 9 = 7
+7 x 2 mod 9 = 5
+7 x 3 mod 9 = 3
+7 x 4 mod 9 = 1
+
+D) 16 mod 9
+16 x 0 mod 9 = 0
+16 x 1 mod 9 = 7
+16 x 2 mod 9 = 5
+16 x 3 mod 9 = 3
+16 x 4 mod 9 = 1
+
+## Part 3: B
+
+A) 0 mod 9
+0 x 0 mod 9 = 0
+0 x 1 mod 9 = 0
+0 x 2 mod 9 = 0
+0 x 3 mod 9 = 0
+0 x 4 mod 9 = 0
+0 x 5 mod 9 = 0
+0 x 6 mod 9 = 0
+0 x 7 mod 9 = 0
+0 x 8 mod 9 = 0
+
+B)6 mod 9
+Both the numbers 6 and 9 share the prime number 3.
+
+C)18 mod 9
+Both the numbers 18 an 9 share the prime number 3.
+
